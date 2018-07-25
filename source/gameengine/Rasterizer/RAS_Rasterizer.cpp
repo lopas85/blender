33ee4c466b4cab6eeae0c869e489e2fe12461af0
Porteries Tristan <republicthunderbolt9@gmail.com>
2018-07-25 19:47:21 +0200

    UPBGE: Fix parallel creation of KX_Scene.
    Previously KX_Scene was parallelly created when libloading in asynchronous mode.
    But the constructor of KX_Scene is not adapted contrary to the conversion
    of the scene data.
    The first issue was related to the debug drawer that was initializing VBOs
    and VAOs, which is totally forbidden in a thread without OpenGL context.
    The second issue is the creation of the task pool using the engine task
    scheduler.
    
    To prevent any futur issues and as the constructor of the scene is cheap,
    its construction is moved in BL_Converter::LinkBlendFile when preparing
    the libload status. All the scenes are created via KX_KetsjiEngine::CreateScene
    and put in a list in KX_LibLoadStatus, this same list is read back in
    async_convert to call the conversion.
    
    Even if the debug draw (RAS_DebugDraw) construction is safe, its implementation
    RAS_OpenGLDebugDraw is not needed per scene as for each update all the
    VBOs are updated. To avoid duplicating simple VBO (e.g cube vertices and
    indices) per scene, only one implementation is stored in the rasterizer
    and the function FlushDebug permit to update the implementation VBO with
    the debug draw to render.

--- source/gameengine/Rasterizer/RAS_Rasterizer.cpp
+++ source/gameengine/Rasterizer/RAS_Rasterizer.cpp
@@ -31,6 +31,7 @@
 
 #include "RAS_Rasterizer.h"
 #include "RAS_OpenGLRasterizer.h"
+#include "RAS_OpenGLDebugDraw.h"
 #include "RAS_IPolygonMaterial.h"
 #include "RAS_DisplayArrayBucket.h"
 
@@ -216,6 +217,7 @@ RAS_Rasterizer::RAS_Rasterizer()
 	m_overrideShader(RAS_OVERRIDE_SHADER_NONE)
 {
 	m_impl.reset(new RAS_OpenGLRasterizer(this));
+	m_debugDrawImpl.reset(new RAS_OpenGLDebugDraw());
 
 	m_numgllights = m_impl->GetNumLights();
 
@@ -1379,6 +1381,11 @@ void RAS_Rasterizer::GetTransform(float *origmat, int objectdrawmode, float mat[
 			memcpy(mat, origmat, sizeof(float) * 16);
 		}
 	}
+}
+
+void RAS_Rasterizer::FlushDebug(RAS_ICanvas *canvas, RAS_DebugDraw *debugDraw)
+{
+	m_debugDrawImpl->Flush(this, canvas, debugDraw);
 }
 
 void RAS_Rasterizer::DisableForText()
